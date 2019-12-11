ea5a2c3fc380edc0480b1835ac832a2dbdafd269
DreamLee0201 <595394202@qq.com>
2019-12-11 23:24:52 +0800

    first commit

---
+++ src/main/java/com/dreamlee/durian/DurianApplication.java
@@ -0,0 +1,13 @@
+package com.dreamlee.durian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DurianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DurianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dreamlee/durian/DurianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamlee.durian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DurianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
